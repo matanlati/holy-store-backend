977495395a5b877896261edd5424c4695ef77386
matanlati <matan11118@gmail.com>
2025-10-26 17:41:11 +0200

    refctor backend

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ app.get('/', (c) => c.text('Hello from Worker + D1!'));
 
 app.get('/api/items', async (c) => {
   const { results } = await c.env.DB.prepare('select *  from items').all();
-  return c.json({"data":results});
+  return c.json(results);
 });
 
 app.post('/api/item', async (c) => {
@@ -33,7 +33,11 @@ app.get('/api/categories', async (c) => {
 
 app.get('/api/storeInfo', async (c) => {
   const { results } = await c.env.DB.prepare('select * from storeInfo').all();
-  return c.json(results);
+  results[0]["contact"]={"phone":results[0]["phone"],"email":results[0]["email"],"address":results[0]["address"]};
+  delete results[0]["phone"];
+  delete results[0]["email"];
+  delete results[0]["address"];
+  return c.json(results[0]);
 });
 
 app.get('/api/storeInfo/gallery', async (c) => {
